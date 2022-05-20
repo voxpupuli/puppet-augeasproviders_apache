e1f1dfddcc7b624c13574999b1a2eb21854be192
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-05-20 11:32:57 +0200

    Minimize spec_helper_local workarounds

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-dir = __dir__
-
 require 'augeas_spec'
 
 # augeasproviders: setting $LOAD_PATH to work around broken type autoloading'
-$LOAD_PATH.unshift(
-  dir,
-  File.join(dir, 'fixtures/modules/augeasproviders_core/spec/lib'),
-  File.join(dir, 'fixtures/modules/augeasproviders_core/lib')
-)
+$LOAD_PATH.unshift(File.join(__dir__, 'fixtures/modules/augeasproviders_core/lib'))
