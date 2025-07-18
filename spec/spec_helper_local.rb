3b0f39d31c27c8eef433d7f2fd508d23ed164a7b
Christoph Maser <christoph.maser+github@gmail.com>
2025-07-18 17:46:21 +0200

    fix: tests

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'augeas_spec'
+require 'fixtures/modules/augeasproviders_core/spec/support/spec/psh_fixtures'
 
 # augeasproviders: setting $LOAD_PATH to work around broken type autoloading'
 $LOAD_PATH.unshift(File.join(__dir__, 'fixtures/modules/augeasproviders_core/lib'))
